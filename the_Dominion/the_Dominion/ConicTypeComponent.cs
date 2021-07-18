24bda2a0dd1736be8e5f1ae1959f12c895122199
bulgos <dchristev@gmail.com>
2021-07-18 17:14:07 +1200

    added Conic Section Type component to validate curves created

---
+++ the_Dominion/the_Dominion/ConicTypeComponent.cs
@@ -0,0 +1,41 @@
+﻿using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace the_Dominion
+{
+    public class ConicTypeComponent : GH_Component
+    {
+        public ConicTypeComponent()
+          : base("ConicType", "ConType",
+              "Tests a curve to see if it is a valid type of conic section",
+              "Dominion", "Utility")
+        { }
+
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddCurveParameter("Curve", "C", "Curve to Test", GH_ParamAccess.item);
+        }
+
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("Conic Type", "T", "Type of Conic section", GH_ParamAccess.item);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            Curve curve = null;
+            DA.GetData(0, ref curve);
+
+            ConicSectionType conicType = curve.GetConicSectionType();
+
+            DA.SetData(0, conicType.ToString());
+        }
+
+        public override Guid ComponentGuid => new Guid("786ab720-39a8-4694-9d08-be1ae47aa59d");
+    }
+}
