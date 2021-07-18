b72a9cee11bb315ddb01d94a97ef7264df1b3e7c
bulgos <dchristev@gmail.com>
2021-07-18 15:42:24 +1200

    added basic Parabola component

--- the_Dominion/the_Dominion/Parabola.cs
+++ the_Dominion/the_Dominion/Parabola.cs
@@ -9,11 +9,23 @@ namespace the_Dominion
 {
     public class Parabola
     {
-        public Curve ParabolaCurve { get; private set; }
-
-        private void ConstructParabola()
+        public static NurbsCurve ConstructParabola(double a)
         {
+            Interval interval = new Interval(-1, 1);
 
+            return ConstructParabola(a, interval);
+        }
+
+        public static NurbsCurve ConstructParabola(double a, Interval interval)
+        {
+            double y0 = a * Math.Pow(interval.Min, 2);
+            double y2 = a * Math.Pow(interval.Max, 2);
+
+            Point3d p0 = new Point3d(interval.Min, y0, 0);
+            Point3d p1 = new Point3d();
+            Point3d p2 = new Point3d(interval.Max, y2, 0);
+
+            return NurbsCurve.CreateParabolaFromVertex(p1, p0, p2);
         }
     }
 }
--- the_Dominion/the_Dominion/ParabolaComponent.cs
+++ the_Dominion/the_Dominion/ParabolaComponent.cs
@@ -10,6 +10,78 @@ using System.Collections.Generic;
 
 namespace the_Dominion
 {
+    public class ParabolaComponent : GH_Component
+    {
+        /// <summary>
+        /// Each implementation of GH_Component must provide a public 
+        /// constructor without any arguments.
+        /// Category represents the Tab in which the component will appear, 
+        /// Subcategory the panel. If you use non-existing tab or panel names, 
+        /// new tabs/panels will automatically be created.
+        /// </summary>
+        public ParabolaComponent()
+          : base("ConstructParabola", "CPrb",
+              "Constructs a Parabola in the form y = ax^2",
+              "Dominion", "Math")
+        { }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddNumberParameter("a", "a", "a", GH_ParamAccess.item, 1);
+            pManager.AddIntervalParameter("Domain", "D", "The Domain to calculate the function in", GH_ParamAccess.item, new Interval(-1, 1));
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddCurveParameter("Parabola", "P", "The resulting Parabola", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
+        /// to store data in output parameters.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            double a = double.NaN;
+            Interval interval = Interval.Unset;
+
+            DA.GetData(0, ref a);
+            DA.GetData(1, ref interval);
+
+            NurbsCurve parabola = Parabola.ConstructParabola(a, interval);
+
+            DA.SetData(0, parabola);
+        }
+
+        /// <summary>
+        /// Provides an Icon for every component that will be visible in the User Interface.
+        /// Icons need to be 24x24 pixels.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                // You can add image files to your project resources and access them like this:
+                //return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Each component must have a unique Guid to identify it. 
+        /// It is vital this Guid doesn't change otherwise old ghx files 
+        /// that use the old ID will partially fail during loading.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("de9bbb6d-cb79-4db8-94ee-48d9045f34b0");
+    }
+
     public class ParabolaFromFocusComponent : GH_Component
     {
         /// <summary>
