ac52fb8b8ce7fa0c4010e52e40b777e853c68c2f
bulgos <dchristev@gmail.com>
2021-07-19 00:36:06 +1200

    added Circle Math tools

---
+++ the_Dominion/the_Dominion/CircleMath.cs
@@ -0,0 +1,57 @@
+﻿using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace the_Dominion.Circles
+{
+    public class CircleMath
+    {
+        // reference for tangent circles 
+        // https://math.stackexchange.com/questions/719758/inner-tangent-between-two-circles-formula
+
+        public static Line[] GetOuterTangets(Circle circle1, Circle circle2)
+        {
+            double r3 = circle1.Radius - circle2.Radius;
+
+            double hypotenuse = circle1.Center.DistanceTo(circle2.Center);
+            double shortSide = r3;
+
+            double angle1 = Math.Atan2(circle2.Center.Y - circle1.Center.Y, circle2.Center.X - circle1.Center.X) + Math.Acos(shortSide / hypotenuse);
+            double angle2 = Math.Atan2(circle2.Center.Y - circle1.Center.Y, circle2.Center.X - circle1.Center.X) - Math.Acos(shortSide / hypotenuse);
+
+            Point3d t1 = circle1.PointAt(angle1);
+            Point3d t2 = circle2.PointAt(angle1);
+
+            Point3d t3 = circle1.PointAt(angle2);
+            Point3d t4 = circle2.PointAt(angle2);
+
+            Line line1 = new Line(t1, t2);
+            Line line2 = new Line(t3, t4);
+
+            return new Line[] { line1, line2 };
+        }
+
+        public static Line[] GetInnerTangents(Circle circle1, Circle circle2)
+        {
+            double hypotenuse = circle1.Center.DistanceTo(circle2.Center);
+            double shortSide = circle1.Radius + circle2.Radius;
+
+            double angle1 = Math.Atan2(circle2.Center.Y - circle1.Center.Y, circle2.Center.X - circle1.Center.X) + Math.Asin(shortSide / hypotenuse) - Math.PI / 2;
+            double angle2 = Math.Atan2(circle2.Center.Y - circle1.Center.Y, circle2.Center.X - circle1.Center.X) - Math.Asin(shortSide / hypotenuse) + Math.PI / 2;
+
+            Point3d t1 = circle1.PointAt(angle1);
+            Point3d t2 = circle2.PointAt(angle1 + Math.PI);
+
+            Point3d t3 = circle1.PointAt(angle2);
+            Point3d t4 = circle2.PointAt(angle2 + Math.PI);
+
+            Line line1 = new Line(t1, t2);
+            Line line2 = new Line(t3, t4);
+
+            return new Line[] { line1, line2 };
+        }
+    }
+}
---
+++ the_Dominion/the_Dominion/GetOuterTangents.cs
@@ -0,0 +1,46 @@
+﻿using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace the_Dominion.Circles
+{
+    public class GetOuterTangents : GH_Component
+    {
+        public GetOuterTangents()
+            : base("OuterTangent", "OTan", 
+                  "Computes the outer Tangents between two circles",
+                  "Dominion", "Circles") { }
+
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddCircleParameter("Circle1", "C1", "The first Circle", GH_ParamAccess.item);
+            pManager.AddCircleParameter("Circle2", "C2", "The second Circle", GH_ParamAccess.item);
+        }
+
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddLineParameter("Tangent1", "T1", "The first Tangent", GH_ParamAccess.item);
+            pManager.AddLineParameter("Tangent2", "T2", "The second Tangent", GH_ParamAccess.item);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            Circle circle1 = Circle.Unset;
+            Circle circle2 = Circle.Unset;
+
+            DA.GetData(0, ref circle1);
+            DA.GetData(1, ref circle2);
+
+            var tangents = CircleMath.GetOuterTangets(circle1, circle2);
+
+            DA.SetData(0, tangents[0]);
+            DA.SetData(1, tangents[1]);
+        }
+
+        public override Guid ComponentGuid => new Guid("3df2a651-3a44-41b5-ae8b-2df306fd9cac");
+    }
+}
