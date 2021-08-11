fd07ecb67716b4c6c5b6d7d4f6bedd1117b13b94
bulgos <dchristev@gmail.com>
2021-08-11 15:49:51 +1200

    remove component wizard comments of hyperbola and parabola.

--- src/Conics/Components/HyperbolaComponent.cs
+++ src/Conics/Components/HyperbolaComponent.cs
@@ -5,31 +5,16 @@ using System.Drawing;
 using the_Dominion.Conics.Wrappers;
 using the_Dominion.Properties;
 
-// In order to load the result of this wizard, you will also need to
-// add the output bin/ folder of this project to the list of loaded
-// folder in Grasshopper.
-// You can use the _GrasshopperDeveloperSettings Rhino command for that.
-
 namespace the_Dominion.Conics.Components
 {
     public class HyperbolaComponent : GH_Component
     {
-        /// <summary>
-        /// Each implementation of GH_Component must provide a public 
-        /// constructor without any arguments.
-        /// Category represents the Tab in which the component will appear, 
-        /// Subcategory the panel. If you use non-existing tab or panel names, 
-        /// new tabs/panels will automatically be created.
-        /// </summary>
         public HyperbolaComponent()
           : base("Construct Hyperbola", "ConHyperb",
               "Constructs a Hyperbola in the form x^2 / a^2 - y^2 / b^2 = 1",
               "Dominion", "Conics")
         { }
 
-        /// <summary>
-        /// Registers all the input parameters for this component.
-        /// </summary>
         protected override void RegisterInputParams(GH_InputParamManager pManager)
         {
             pManager.AddPlaneParameter("Plane", "P", "Plane in which to create Hyperbola", GH_ParamAccess.item, Plane.WorldXY);
@@ -40,19 +25,11 @@ namespace the_Dominion.Conics.Components
                 "if true, Hyperbola will be created in the form y^2 / b^2 - x^2 / a^2 = 1", GH_ParamAccess.item, false);
         }
 
-        /// <summary>
-        /// Registers all the output parameters for this component.
-        /// </summary>
         protected override void RegisterOutputParams(GH_OutputParamManager pManager)
         {
             pManager.AddParameter(new Conic_Param(), "Hyperbola", "H", "The resulting Hyperbola", GH_ParamAccess.item);
         }
 
-        /// <summary>
-        /// This is the method that actually does the work.
-        /// </summary>
-        /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
-        /// to store data in output parameters.</param>
         protected override void SolveInstance(IGH_DataAccess DA)
         {
             Plane plane = Plane.Unset;
--- src/Conics/Components/ParabolaComponent.cs
+++ src/Conics/Components/ParabolaComponent.cs
@@ -5,50 +5,27 @@ using System.Drawing;
 using the_Dominion.Conics.Wrappers;
 using the_Dominion.Properties;
 
-// In order to load the result of this wizard, you will also need to
-// add the output bin/ folder of this project to the list of loaded
-// folder in Grasshopper.
-// You can use the _GrasshopperDeveloperSettings Rhino command for that.
-
 namespace the_Dominion.Conics.Components
 {
     public class ParabolaComponent : GH_Component
     {
-        /// <summary>
-        /// Each implementation of GH_Component must provide a public 
-        /// constructor without any arguments.
-        /// Category represents the Tab in which the component will appear, 
-        /// Subcategory the panel. If you use non-existing tab or panel names, 
-        /// new tabs/panels will automatically be created.
-        /// </summary>
         public ParabolaComponent()
           : base("Construct Parabola", "ConParab",
               "Constructs a Parabola in the form y = ax^2",
               "Dominion", "Conics")
         { }
 
-        /// <summary>
-        /// Registers all the input parameters for this component.
-        /// </summary>
         protected override void RegisterInputParams(GH_InputParamManager pManager)
         {
             pManager.AddNumberParameter("a", "a", "a", GH_ParamAccess.item, 1);
             pManager.AddIntervalParameter("Domain", "D", "The Domain to calculate the function in", GH_ParamAccess.item, new Interval(-10, 10));
         }
 
-        /// <summary>
-        /// Registers all the output parameters for this component.
-        /// </summary>
         protected override void RegisterOutputParams(GH_OutputParamManager pManager)
         {
             pManager.AddParameter(new Conic_Param(), "Parabola", "P", "The resulting Parabola", GH_ParamAccess.item);
         }
 
-        /// <summary>
-        /// This is the method that actually does the work.
-        /// </summary>
-        /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
-        /// to store data in output parameters.</param>
         protected override void SolveInstance(IGH_DataAccess DA)
         {
             double a = double.NaN;
