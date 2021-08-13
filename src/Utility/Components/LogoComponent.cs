a6a111770be05db26594401e8e85237917e57454
bulgos <dchristev@gmail.com>
2021-08-13 17:35:43 +1200

    add logo gh_file

--- src/Utility/Components/LogoComponent.cs
+++ src/Utility/Components/LogoComponent.cs
@@ -19,7 +19,7 @@ namespace the_Dominion.Utility.Components
         {
             pManager.AddPlaneParameter("Plane", "P", "Plane of Orientation", GH_ParamAccess.item);
             pManager.AddNumberParameter("Ratio", "R", "Height/Width Ratio of Logo", GH_ParamAccess.item, 1.0);
-            pManager.AddNumberParameter("Thickness", "T", "Animation Thickness of Logo", GH_ParamAccess.item, 0.2);
+            pManager.AddNumberParameter("Thickness", "T", "Thickness of Logo", GH_ParamAccess.item, 0.2);
             pManager.AddNumberParameter("Borders", "B", "Border curves of Logo", GH_ParamAccess.list);
 
             pManager[3].Optional = true;
